54bab409d5edfaf864c0f77ed0489e4badfefd13
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2023-12-01 16:57:45 -0500

    Remove failing cases in tests

--- 2017/days/day1_test.go
+++ 2017/days/day1_test.go
@@ -8,11 +8,9 @@ import (
 func TestDay1Part1Example1(t *testing.T) {
 	path := filepath.Join("..", "puzzles", "day1-sample1.txt")
 	Day1Part12(path)
-	t.Errorf("failed")
 }
 
 func TestDay1Part1Input1(t *testing.T) {
 	path := filepath.Join("..", "puzzles", "day1-input1.txt")
 	Day1Part12(path)
-	t.Errorf("failed")
 }
